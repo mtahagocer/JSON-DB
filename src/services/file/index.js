12d29f70768a8b39881621513828a1fb1d567d9c
Taha GÖÇER <taha.gocer@akinon.com>
2021-03-15 23:51:40 +0300

    file service update for entitys

--- src/services/file/index.js
+++ src/services/file/index.js
@@ -1,16 +1,18 @@
 import * as fs from 'fs';
 import * as path from 'path';
-
 class File {
+    static resolveBasePath = (destinationPath) => path.join(__dirname, '../../../', destinationPath)
     static isExist(sourcePath) {
         return fs.existsSync(sourcePath);
     }
 
     static async checkDirectory(destination) {
-        const directory = path.join(__dirname, destination);
-
+        const directory = this.resolveBasePath(destination);
+        console.log({ directory });
         if (!this.isExist(directory)) {
-            await fs.mkdir(directory, { recursive: true });
+            await fs.mkdir(directory, { recursive: true }, (err) => {
+                if (err) throw err;
+            });
         }
     }
 
@@ -20,36 +22,40 @@ class File {
 
     static async copyFile(source, destination) {
         await this.checkDirectory(destination);
-        await fs.copyFile(source, destination);
+        await fs.copyFile(this.resolveBasePath(source), this.resolveBasePath(destination));
     }
 
     static async replaceInFile(sourcePath, destinationPath, replacements = []) {
         await this.checkDirectory(destinationPath);
-        let file = fs.readFileSync(sourcePath, 'utf8');
+        let file = fs.readFileSync(this.resolveBasePath(sourcePath), 'utf8');
 
         replacements.forEach(({ oldContent, newContent }) => {
             file = file.replace(oldContent, newContent);
         });
-        return fs.writeFile(destinationPath, file);
+        return fs.writeFile(this.resolveBasePath(destinationPath), file, (err) => {
+            if (err) throw err;
+        });
     }
 
     static async applyPatch(sourcePath, params) {
         const { pattern, patch } = params;
 
-        if (!await this.readFile(sourcePath).includes(patch)) {
+        if (!await this.readFile(this.resolveBasePath(sourcePath)).includes(patch)) {
             await fs.writeFile(
-                sourcePath,
+                this.resolveBasePath(sourcePath),
                 fs.readFileSync(sourcePath, 'utf8').replace(pattern, (match) => `${match}${patch}`)
             );
         }
     }
 
     static async writeFile(filePath, content) {
-        await fs.writeFile(filePath, content);
+        await fs.writeFile(this.resolveBasePath(filePath), content, (err) => {
+            if (err) throw err;
+        });
     }
 
     static async mkdir(filePath) {
-        await fs.mkdir(path.join(filePath), { recursive: true });
+        await fs.mkdir(this.resolveBasePath(filePath), { recursive: true });
     }
 
 }
