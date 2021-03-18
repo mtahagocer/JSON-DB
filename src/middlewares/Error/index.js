21b34483c61047de68ecc3e406a3586f278e1c6c
Taha GÖÇER <taha.gocer@akinon.com>
2021-03-18 23:56:08 +0300

    Error Middleware

--- src/middlewares/Error/index.js
+++ src/middlewares/Error/index.js
@@ -1,10 +1,16 @@
-export default function (err, req, res, next) {
-    console.error({ err });
+export default function (err, req, res) {
     if (err.name === 'UnauthorizedError') {
         return res.status(401).json({
             success: false,
             error: ['Invalid token']
         });
     }
-    next();
+
+    const { status, message } = err;
+
+    return res.status(status).json({
+        success: false,
+        status,
+        message,
+    });
 }
