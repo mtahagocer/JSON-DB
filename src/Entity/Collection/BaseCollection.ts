39e4979d6afe5dc97b81d21d2fedfc4b13a3b012
Taha GÖÇER <taha.gocer@akinon.com>
2021-03-18 23:55:44 +0300

    BaseCollection Get-Post-Delete

--- src/Entity/Collection/BaseCollection.ts
+++ src/Entity/Collection/BaseCollection.ts
@@ -1,38 +1,45 @@
 import * as utils from '../../Utils';
 import BaseDocument from '../Document/BaseDocument';
-
+import CustomError from '../CustomError';
 export default class BaseCollection {
-    private Id: string;
     private Name: string;
-    Documents: Array<BaseDocument> = [];
+    Documents: Array<BaseDocument>
 
     private CreationDate: Date;
     private UpdateDate: Date;
+    private UserId: string;
 
-    constructor() {
-        this.Id = utils.generateId();
-        this.CreationDate = new Date(Date.now());
+    constructor({ UserId, Name = '', Documents = [] }: BaseCollection) {
+        if (!Name) throw new CustomError('Collection Name is required');
+
+        this.Name = Name;
+        this.Documents = Documents;
+        this.UserId = UserId;
     }
 
-    Save = async () => {
+    Get = async () => {
+        return await utils.getCollection({ ...this });
+    }
+
+    SaveCollection = async () => {
+        this.CreationDate = new Date(Date.now());
+        await utils.createCollection({ ...this });
+        this.Documents.map(async (item) => {
+            item.Id = utils.generateId();
+            item.CreationDate = new Date(Date.now());
+            await item.SaveDocument(item, this.Name);
+        });
+    }
+
+    Update = async () => {
         await utils.updateCollection({ ...this });
     }
 
-    SaveCollection = async (collectionRef: Object) => {
-        if (!this.Name) {
-            this.Name = collectionRef.constructor.name;
-            await utils.createCollection(this.Name);
-
-            await this.Documents.map(async (item) => {
-                item.Id = utils.generateId();
-                item.CreationDate = new Date(Date.now());
-                await item.SaveDocument(item, this.Name);
-            });
-        }
+    Delete = async (force) => {
+        await utils.deleteCollection({ ...this }, force);
     }
 
     addDocument = async (Document: BaseDocument) => {
-        if (this.Documents.find((document) => document.Name === Document.Name)) throw new Error(`There is a already Document with ${Document.Name} name`);
         this.Documents.push(Document);
     }
 
