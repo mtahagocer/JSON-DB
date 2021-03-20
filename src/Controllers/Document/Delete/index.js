3875d298926c3da01d9197762ea5ddb1124d5728
Taha GÖÇER <taha.gocer@akinon.com>
2021-03-20 22:34:04 +0300

    Document Post && Update collection after document delete && Post

--- src/Controllers/Document/Delete/index.js
+++ src/Controllers/Document/Delete/index.js
@@ -1,4 +1,5 @@
 import BaseDocument from '../../../Entity/Document/BaseDocument';
+import BaseCollection from '../../../Entity/Collection/BaseCollection';
 import CustomError from '../../../Entity/CustomError';
 import asyncHandler from 'express-async-handler';
 import { filterByKeyAndValue } from '../../../Helpers';
@@ -11,11 +12,16 @@ export default asyncHandler(async (req, res) => {
     if (KeyList.some((item) => typeof item !== 'string')) throw new CustomError('"KeyList items" must be a string object key.');
 
     const _document = new BaseDocument(CollectionName);
+    const _collection = new BaseCollection({ UserId: id, Name: CollectionName });
 
     const filter = filterByKeyAndValue(KeyList, ValueList);
     const _data = await _document.Delete(id, filter);
     const status = _data.length > 0 ? 200 : 404;
-
+    await _collection.Update(await _collection.Get().then((response) => {
+        response.DocumentCount -= _data.length;
+        return response;
+    }));
+    await
     res.status(status).json({
         success: true,
         DocumentCount: _data.length,
---
+++ src/Controllers/Document/Post/index.js
@@ -0,0 +1,24 @@
+import BaseDocument from '../../../Entity/Document/BaseDocument';
+import BaseCollection from '../../../Entity/Collection/BaseCollection';
+import CustomError from '../../../Entity/CustomError';
+import asyncHandler from 'express-async-handler';
+
+export default asyncHandler(async (req, res) => {
+    const { user: { id } } = req;
+    const { CollectionName, Params } = req.body;
+
+    if (!Params) throw new CustomError('"Params" is required');
+
+    const _document = new BaseDocument(CollectionName, Params);
+    const _collection = new BaseCollection({ UserId: id, Name: CollectionName });
+    await _collection.Update(await _collection.Get().then((response) => {
+        response.DocumentCount += 1;
+        return response;
+    }));
+    const _data = await _document.SaveDocument(id);
+    res.json({
+        success: true,
+        message: 'Document saved successfully',
+        Document: _data
+    });
+});
