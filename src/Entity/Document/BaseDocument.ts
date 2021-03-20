2a49057eb20aa60af713a3c49bf2fa155adda63e
Taha GÖÇER <taha.gocer@akinon.com>
2021-03-20 13:23:06 +0300

    BaseDocument CRUD abstraction

--- src/Entity/Document/BaseDocument.ts
+++ src/Entity/Document/BaseDocument.ts
@@ -1,25 +1,41 @@
-import * as utils from '../../Utils';
+import * as DocumentActions from '../../Actions/Document';
+import * as Helpers from '../../Helpers';
+import CustomError from '../CustomError';
 
 export default class BaseDocument {
     Id: string;
+    CollectionName: string;
     Name: string;
     CreationDate: Date;
     UpdateDate: Date;
+    Params: Object = {};
 
-    constructor() {
+    constructor({ CollectionName, Params }: BaseDocument) {
+        if (!CollectionName) throw new CustomError('Collection Name is required');
+        this.CollectionName = CollectionName;
+        if (Params) this.Params = Params;
+    }
+
+    Get = async (UserId: string, filter: Function) => {
+        return await DocumentActions.getDocument(UserId, this.CollectionName, filter);
+    }
+
+    SaveDocument = async (UserId, Name: string) => {
         this.CreationDate = new Date(Date.now());
+        this.Id = Helpers.generateId();
+        this.Name = Name;
+        return await DocumentActions.createDocument(UserId, this.CollectionName, { ...this });
     }
 
-    Save = async () => {
-        await utils.updateDocument({ ...this });
+    Update = async (UserId: string, Id: string, replace: boolean) => {
+        if (!Id) throw new CustomError('Document Id is required');
+        this.UpdateDate = new Date(Date.now());
+        await DocumentActions.updateDocument(UserId, this.CollectionName, { ...this }, replace);
     }
 
-    SaveDocument = async (documentRef: Object, CollectionName: string) => {
-        console.log({ documentRef });
-        if (!this.Name) {
-            this.Id = utils.generateId();
-            this.Name = documentRef.constructor.name;
-            await utils.createDocument(CollectionName, this);
-        }
+    Delete = async (UserId: string, Id: string) => {
+        if (!Id) throw new CustomError('Document Id is required');
+        await DocumentActions.deleteDocument(UserId, this.CollectionName, this.Id);
     }
+
 }
