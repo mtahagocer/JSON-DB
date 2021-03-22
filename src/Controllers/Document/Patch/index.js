8fda6a254437276ca492803175a577dc1343ecc4
Taha GÖÇER <taha.gocer@akinon.com>
2021-03-22 22:39:40 +0300

    Multiple search algorithm support on Patch Document

--- src/Controllers/Document/Patch/index.js
+++ src/Controllers/Document/Patch/index.js
@@ -1,19 +1,21 @@
-import BaseDocument from '../../../Entity/Document/BaseDocument';
+import BaseCollection from '../../../Entity/Collection/BaseCollection';
 import CustomError from '../../../Entity/CustomError';
 import asyncHandler from 'express-async-handler';
-import { filterByPatch } from '../../../Helpers';
+import { filterByPatch, filterByKeyAndValue } from '../../../Helpers';
 
 export default asyncHandler(async (req, res) => {
     const { User: { Id } } = req;
-    const { Patch, CollectionName, DeepEquality = false } = req.body;
+    const { CollectionName, Patch, DeepEquality = false, KeyList, ValueList, Strict = true } = req.body;
 
-    if (!Patch) throw new CustomError('"Patch" is required');
-    if (!Object.keys(Patch).length) throw new CustomError('"Patch" must be a object');
+    if ((!Patch && ((!KeyList && ValueList) || (KeyList && !ValueList))) || (Patch && (KeyList || ValueList))) throw new CustomError(' "Patch" OR "KeyList and ValueList" is required');
+    if (Strict !== undefined && typeof Strict !== 'boolean') throw new CustomError('"Strict" must be a boolean');
+    if (Patch && !Object.keys(Patch).length) throw new CustomError('"Patch" must be a object');
+    if ((KeyList && !Array.isArray(KeyList)) || (ValueList && !Array.isArray(ValueList))) throw new CustomError('"KeyList" and "ValueList" must be a array');
+    if (KeyList && KeyList.some((item) => typeof item !== 'string')) throw new CustomError('"KeyList items" must be a string object key.');
 
-    const _document = new BaseDocument(CollectionName, Patch);
+    const filter = Patch ? filterByPatch(Patch, DeepEquality) : filterByKeyAndValue(KeyList, ValueList)(Strict);
+    const _data = await new BaseCollection({ UserId: Id, Name: CollectionName }).GetDocument(filter);
 
-    const filter = filterByPatch(Patch, DeepEquality);
-    const _data = await _document.Get(Id, filter);
     const status = _data.length > 0 ? 200 : 404;
 
     res.status(status).json({
