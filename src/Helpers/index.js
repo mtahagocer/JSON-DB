65ee8438c14c95cf05a8de8a9a9a5ec6975e64f8
Taha GÖÇER <taha.gocer@akinon.com>
2021-03-25 22:16:18 +0300

    create token

--- src/Helpers/index.js
+++ src/Helpers/index.js
@@ -1,6 +1,19 @@
+import 'dotenv/config';
 import * as uuid from 'uuid';
-import paths from '../Constants/Path';
+import Paths from '../Constants/Path';
+import Types from '../Constants/Type';
 import CustomError from '../Entity/CustomError';
+import jwt from 'jsonwebtoken';
+
+export const createToken = (data, expiresIn = process.env.JWT_EXPIRE_TIME) => new Promise((resolve, reject) => {
+    jwt.sign(data, process.env.JWT_SECRET, {
+        algorithm: process.env.JWT_ALGORITHM,
+        ...(expiresIn ? ({ expiresIn: expiresIn }) : {})
+    }, (err, token) => {
+        if (err) return reject(err);
+        return resolve(token);
+    });
+});
 
 export const required = (message, status) => {
     throw new CustomError(message, status);
@@ -8,9 +21,12 @@ export const required = (message, status) => {
 
 export const generateId = () => uuid.v4();
 
-export const stringify = (data) => JSON.stringify(data, null, 2);
+export const isProduction = process.env.NODE_ENV === 'production';
 
-export const dbPath = (path) => `${paths.db}/${path}`;
+export const stringify = (data) => JSON.stringify(data, null, isProduction ? 0 : 2);
+
+
+export const dbPath = (path) => `${Paths.db}/${path}`;
 
 export const deepEqual = (x, y) => {
     const keys = Object.keys, tx = typeof x, ty = typeof y;
@@ -25,17 +41,18 @@ export const someEqual = (x, y, excludeKeyList = []) => {
     return x && y && tx === 'object' && tx === ty ? (
         keys(x).some((key) => excludeKeyList.indexOf(key) === -1 && deepEqual(x[key], y[key]))
     ) : (() => {
-        // TODO: logger
-        x === y && console.log({ x, y });
+        // x === y && console.log({ x, y });
         return x === y;
     })();
 };
 
-const keys = Object.keys;
-const is = (first, second) => first !== undefined && second !== undefined;
 
 export const filterByPatch = (Patch, DeepEquality) => (dbData) => {
+
+    const keys = Object.keys;
+    const is = (first, second) => first !== undefined && second !== undefined;
     let isMatch = false;
+
     keys(Patch).forEach((key) => {
         const dbObject = dbData[key] || {};
         const patchObject = Patch[key] || {};
@@ -50,11 +67,16 @@ export const filterByPatch = (Patch, DeepEquality) => (dbData) => {
     return isMatch;
 };
 
-export const filterByKeyAndValue = (KeyList, ValueList) => (opposite = true) => (dbData) => {
-    let isMatch = !opposite;
+export const filterByKeyAndValue = (KeyList, ValueList) => (strict = true) => (dbData) => {
+    let isMatch = !strict;
     KeyList.forEach((key) => {
-        if (ValueList.indexOf(dbData[key]) !== -1) isMatch = opposite;
+        if (ValueList.indexOf(dbData[key]) !== -1) isMatch = strict;
     });
 
     return isMatch;
+};
+
+export const checkType = (data, type) => {
+    if (!Types[type](data)) throw new CustomError(`For ${JSON.stringify(data)} expected type have to be ${type}!`);
+    return true;
 };
