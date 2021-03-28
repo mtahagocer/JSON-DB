3a3c9ebfcd4bc206560546044d676d0beeb394bb
Taha GÖÇER <taha.gocer@akinon.com>
2021-03-28 22:34:14 +0300

    Actions moved in to Business layer

--- src/Business/Collection/index.js
+++ src/Business/Collection/index.js
@@ -1,41 +1,40 @@
 import fs from '../../Services/File';
 import CustomError from '../../Entity/CustomError';
-import asyncHandler from 'express-async-handler';
 import { dbPath, stringify } from '../../Helpers';
 
 export const getCollectionPath = (UserId, Name) => dbPath(`${UserId}/${Name}`);
 
-export const controlCollection = asyncHandler(async (UserId, CollectionName) => {
+export const controlCollection = async (UserId, CollectionName) => {
     if (await !fs.isExist(getCollectionPath(UserId, CollectionName))) throw new CustomError(`There is not any Collection with Name = ${CollectionName}`);
     return true;
-});
+};
 
 export const getCollectionFilePath = (UserId, Name) => dbPath(`${UserId}/${Name}/index.json`);
 
-export const createCollection = asyncHandler(async (Collection) => {
+export const createCollection = async (Collection) => {
     if (fs.isExist(getCollectionPath(Collection.UserId, Collection.Name))) throw new CustomError(`There is a already Collection with Name = ${Collection.Name}`);
 
     await fs.checkDirectory(getCollectionPath(Collection.UserId, Collection.Name));
     await fs.writeFile(getCollectionFilePath(Collection.UserId, Collection.Name), stringify(Collection));
     await fs.writeFile(`${getCollectionPath(Collection.UserId, Collection.Name)}/data.json`, stringify([]));
-});
+};
 
-export const getCollection = asyncHandler(async (Collection) => {
+export const getCollection = async (Collection) => {
     await controlCollection(Collection.UserId, Collection.Name);
     return await fs.readFile(getCollectionFilePath(Collection.UserId, Collection.Name)).then((data) => {
         return data;
     }).catch((err) => {
         throw new CustomError(err.message);
     });
-});
+};
 
-export const updateCollection = asyncHandler(async (Collection) => {
+export const updateCollection = async (Collection) => {
     await fs.writeFile(getCollectionFilePath(Collection.UserId, Collection.Name), stringify(Collection));
-});
+};
 
-export const deleteCollection = asyncHandler(async (Collection, force) => {
+export const deleteCollection = async (Collection, force) => {
     await controlCollection(Collection.UserId, Collection.Name);
     await fs.deleteFile(getCollectionPath(Collection.UserId, Collection.Name), force).catch((err) => {
         throw new CustomError(err.message);
     });
-});
+};
---
+++ src/Business/Document/index.js
@@ -0,0 +1,57 @@
+import fs from '../../Services/File';
+import { getCollectionPath, controlCollection } from '../Collection';
+import { stringify } from '../../Helpers';
+
+export const getDocumentPath = (UserId, CollectionName) => `${getCollectionPath(UserId, CollectionName)}/data.json`;
+
+const _writeDocument = async (UserId, CollectionName, Document) => await fs.writeFile(getDocumentPath(UserId, CollectionName), stringify(Document));
+
+export const getDocument = async (UserId, CollectionName, filter) => {
+    if (await controlCollection(UserId, CollectionName)) {
+        let data = await fs.readFile(getDocumentPath(UserId, CollectionName));
+
+        if (filter) {
+            data = data.filter(filter);
+        }
+        return data;
+    }
+};
+
+export const createDocument = async (UserId, CollectionName, Document) => {
+    const prev = await getDocument(UserId, CollectionName);
+    const newData = [...prev, Document];
+    await _writeDocument(UserId, CollectionName, newData);
+    return Document;
+};
+
+export const updateDocument = async (UserId, CollectionName, Document, Replace = false) => {
+    let prev = await getDocument(UserId, CollectionName);
+    const docIndex = await prev.findIndex((user) => user._Id === Document._Id);
+
+    if (docIndex === -1) throw new Error(`There is no Document with Id ${Document._Id}`);
+
+    if (Replace) {
+        prev[docIndex] = {
+            ...Document,
+            _CreationDate: prev[docIndex]._CreationDate,
+        };
+    } else {
+        prev[docIndex].Data = {
+            ...prev[docIndex],
+            ...Document,
+            _CreationDate: prev[docIndex]._CreationDate,
+        };
+    }
+
+    await _writeDocument(UserId, CollectionName, prev);
+    return prev[docIndex];
+};
+
+export const deleteDocument = async (UserId, CollectionName, filter) => {
+    let data = await getDocument(UserId, CollectionName);
+    const dataLength = data.length;
+    const filtered = await data.filter(filter);
+    await _writeDocument(UserId, CollectionName, filtered);
+
+    return dataLength - filtered.length;
+};
